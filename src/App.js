9b641b8171098b6057cbba9468d129fff0ac7806
Muhammad Shahzad <muhammad.shahzad@selectquote.com>
2023-10-20 11:13:00 +0500

    guidelines added for git

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { release } from "os";
 
 function App() {
   return <div></div>;
 }
 
 export default App;
+// feature-add-date-picker
+// bugFix-date-not-formated
